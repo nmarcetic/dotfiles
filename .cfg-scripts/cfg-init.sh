6b6d7f8ce8ff36e95aa53f2094a62850eeea00b2
nmarcetic <n.marcetic86@gmail.com>
2019-02-28 12:07:00 +0100

    Fixed init script
    Signed-off-by: nmarcetic <n.marcetic86@gmail.com>

--- .cfg-scripts/cfg-init.sh
+++ .cfg-scripts/cfg-init.sh
@@ -1,4 +1,5 @@
 git init --bare $HOME/.cfg
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+sleep 2
 config config --local status.showUntrackedFiles no
 echo "alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.zshrc
