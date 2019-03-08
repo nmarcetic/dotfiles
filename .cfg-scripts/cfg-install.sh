27b65eacb01dab7bb5b59dc7f65cf3d5b313aa4a
Nikola Marčetić <n.marcetic86@gmail.com>
2019-03-08 14:22:41 +0100

    Update cfg-install.sh

--- .cfg-scripts/cfg-install.sh
+++ .cfg-scripts/cfg-install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@bitbucket.org:nmarcetic/dotfiles.git $HOME/.cfg
+git clone --bare git@github.com:nmarcetic/dotfiles.git $HOME/.cfg
 function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
